0a4f8574de0bc9b2cf345c187dbde5379589e656
Pim Linders <pimlinders@gmail.com>
2017-01-10 12:18:06 -0500

    adds word conversion

--- data/process.js
+++ data/process.js
@@ -1,17 +1,27 @@
 const csv_to_json = require('./csv_to_json');
+const word_to_json = require('./word_to_json')
 const fs = require('fs');
-const csv_path = 'data/thanks_obama_content.csv';
-const json_path = 'data/events.json';
 const async = require('async');
 
+const csv_path = 'data/thanks_obama_content.csv';
+const json_path = 'data/events.json';
+const word_path = "data/Blurbs.docx";
+
 async.waterfall([
+    // function(callback) {
+    //     console.log('Converting csv to json');
+    //     csv_to_json.convert_to_json(csv_path, callback);
+    // },
+    // function(csv_json, callback) {
+    //     console.log('Fetching json assets');
+    //     csv_to_json.fetch_assets(csv_json, callback);
+    // },
+    // // function(csv_json, callback) {
+    // //     console.log('Fetching json assets');
+    // //     csv_to_json.fetch_assets(csv_json, callback);
+    // // },
     function(callback) {
-        console.log('Converting to json');
-        csv_to_json.convert_to_json(csv_path, callback);
-    },
-    function(json, callback) {
-        console.log('Fetching assets');
-        csv_to_json.fetch_assets(json, callback);
+        word_to_json.convert_docx_to_json(word_path, callback);
     },
     function(json) {
         console.log('Exporting to ' + json_path);
--- data/word_to_json.js
+++ data/word_to_json.js
@@ -1,13 +1,99 @@
-const word_path = "Blurbs.docx";
+const mammoth = require("mammoth");
+const fs = require('fs');
+const jsdom = require("jsdom");
+const async = require('async');
 
-var run_mammoth = function(path, callback) {
-    mammoth.convertToHtml({path: path}, options).then(function(result){
-        var html = result.value; // The generated HTML
-        var messages = result.messages; // Any messages, such as warnings during conversion
-        if (messages.length) {
-            console.warn(messages);
+module.exports = {
+
+    convert_docx_to_json: function(word_path, complete_callback) {
+        var _this = this;
+        async.waterfall([
+            function(callback) {
+                console.log('Converting to html');
+                _this.convert_docx_to_html(word_path, callback);
+            },
+            function(html, callback) {
+                console.log('Converting to json assets');
+                var json = _this.convert_html_to_json(html);
+                complete_callback(null, _this.generate_array(json));
+            }
+        ], function(err, results) {
+            console.log('Error', err);
+        });
+    },
+
+    generate_array: function(obj) {
+        var keys = Object.keys(obj),
+            arr = [];
+        for (var i = 0; i < keys.length; i += 1) {
+            var key = keys[i],
+                property = obj[key];
+            arr.push(property);
         }
-        callback(html);
-    })
-    .done();
+        return arr;
+    },
+
+    convert_docx_to_html: function (path, callback) {
+        mammoth.convertToHtml({path: path}, {
+            styleMap: []
+        }).then(function (result) {
+            var html = result.value; // The generated HTML
+            var messages = result.messages; // Any messages, such as warnings during conversion
+            if (messages.length) {
+                console.warn(messages);
+            }
+            callback(null, html);
+        })
+            .done();
+    },
+
+    find_obj_placeholder: function (obj, placeholder = '@') {
+        var keys = Object.keys(obj),
+            placeholder_key = null;
+        for (var i = 0; i < keys.length; i += 1) {
+            var key = keys[i],
+                property = obj[key];
+            if (property === placeholder) {
+                placeholder_key = key;
+                break;
+            }
+        }
+        return placeholder_key;
+    },
+
+    convert_html_to_json: function (html_str) {
+        var _this = this,
+            json = {},
+            document = jsdom.jsdom(html_str),
+            lines = document.querySelectorAll('p'),
+            text_json = null,
+            property_placeholder = null,
+            fetch_next_line = false;
+        for (var i = 0; i < lines.length; i += 1) {
+            var line = lines[i],
+                text = line.innerHTML;
+            if (fetch_next_line) {
+                text_json[property_placeholder] = text;
+                fetch_next_line = false;
+            }
+            // [[ ]] contain json within
+            else if (text.indexOf('[[') !== -1 && text.indexOf(']]') !== -1) {
+                text = text.replace('[[', '');
+                text = text.replace(']]', '');
+                // word has a tendency to use “ and ” for quotes, replace all
+                text = text.replace(new RegExp("“", 'g'), '"');
+                text = text.replace(new RegExp("”", 'g'), '"');
+                // convert text to json
+                text_json = JSON.parse(text);
+                if (text_json.id) {
+                    json[text_json.id] = text_json;
+                    property_placeholder = _this.find_obj_placeholder(text_json);
+                    if (property_placeholder) {
+                        fetch_next_line = true;
+                    }
+                }
+            }
+        }
+        return json;
+    }
 };
